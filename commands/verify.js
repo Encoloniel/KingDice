e84638131462f901442593956f7bca5ef16aeaf5
Louis Nam <junwonnam@naver.com>
2017-12-01 16:34:59 +0900

    verify command fix

--- commands/verify.js
+++ commands/verify.js
@@ -6,12 +6,13 @@ const {
 module.exports = {
   purpose: function(message, client) {
     try {
-      let role = message.guild.roles.find("name", "Verified");
+      var role = message.guild.roles.find("name", "Verified");
       if (!role) {
         message.guild.createRole({
           name: 'Verified',
           color: 'GREEN',
         }).catch(err => message.channel.send(ess.errorHandle(err)))
+        role = message.guild.roles.find("name", "Verified");
       }
       message.guild.member(message.author).addRole(role).catch(err => message.channel.send(ess.errorHandle(err)))
       message.channel.send("Verified!")
