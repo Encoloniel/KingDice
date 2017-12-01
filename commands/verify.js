7dea16cccd7a1e74f590dd97cbc6effcd1fe8175
Louis Nam <junwonnam@naver.com>
2017-12-01 16:41:42 +0900

    .4

--- commands/verify.js
+++ commands/verify.js
@@ -11,6 +11,8 @@ module.exports = {
           name: 'Verified',
           color: 'GREEN',
         }).catch(err => message.channel.send(ess.errorHandle(err)))
+        message.channel.send("There was no Verified role so I created it. Next time you can use =verify to verify yourself!")
+        return;
       }
       let role = message.guild.roles.find("name", "Verified");
       message.guild.member(message.author).addRole(role).catch(err => message.channel.send(ess.errorHandle(err)))
