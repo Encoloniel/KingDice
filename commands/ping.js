c9de7886496bb390d2ee3f40f1dd13c930b0cc2e
Louis Nam <junwonnam@naver.com>
2017-12-03 00:18:16 +0900

    Ping update

--- commands/ping.js
+++ commands/ping.js
@@ -3,15 +3,24 @@ const ess = require('../ess');
 module.exports = {
   purpose: function(message, client) {
     try {
+      let beforedate = Date.now()
       let embedMessage = {
         embed: {
           color: 0xd38cff,
           fields: [{
-              name: "Message Roundtrip",
+              name: "Message -> Code",
               value: `${Math.abs(Date.now() - message.createdTimestamp)} ms`
             },
             {
-              name: "Ping",
+              name: "Code -> Channel",
+              value: "pinging..."
+            },
+            {
+              name: "Message -> Channel",
+              value: "pinging..."
+            },
+            {
+              name: "Avg. Ping",
               value: "pinging..."
             }
           ],
@@ -23,10 +32,14 @@ module.exports = {
         }
       }
       message.channel.send(embedMessage).then(sent => {
-        embedMessage.embed.fields[2].value = sent.createdTimestamp - message.createdTimestamp + "ms"
+        embedMessage.embed.fields[1].value = (sent.createdTimestamp - message.createdTimestamp) + "ms"
+        sent.edit(embedMessage);
+        embedMessage.embed.fields[2].value = Math.abs(beforedate - Date.now()) + "ms"
+        sent.edit(embedMessage);
+        let mf = embedMessage.embed.fields
+        embedMessage.embed.fields[3].value = Math.round((parseInt(mf[0].value) + parseInt(mf[1].value) + parseInt(mf[2].value)) / 3) + "ms"
         sent.edit(embedMessage);
       });
-      message.channel.send(embedMessage);
       return;
     } catch (err) {
       message.channel.send(ess.errorHandle(err));
